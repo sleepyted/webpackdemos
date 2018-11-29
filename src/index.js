69829fcbdc3e8cdadb52f36b9693e50e13f68c1c
matengfei <1341555203@qq.com>
2018-11-29 22:46:27 +0800

    next to load font

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from "lodash"
+import './style.css'
+import icon from './icon.png'
 
 function component(){
     let element = document.createElement('div')
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
+    let img = new Image()
+    img.src = icon
+    element.appendChild(img)
+    
     return element
 }
 
