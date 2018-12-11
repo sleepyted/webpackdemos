7973630c90fc86e67def8486a6b2aa4ba2a211c8
matengfei1 <matengfei1@hisense.com>
2018-12-11 14:15:23 +0800

    next output management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import _ from "lodash"
-import './style.css'
-import icon from './icon.png'
-import Data from './data.xml'
 
 function component(){
     let element = document.createElement('div')
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
-
-    let img = new Image()
-    img.src = icon
-    element.appendChild(img)
     
     return element
 }
-
-console.log(Data)
 
 document.body.appendChild(component())
