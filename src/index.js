4134abe888dc3700fd3be1903a59d797f730b5c0
matengfei1 <matengfei1@hisense.com>
2018-12-11 18:38:24 +0800

    add webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component(){
     let element = document.createElement('div')
     let btn = document.createElement('button')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = _.join(['Hello', 'webpack!'], ' ')
     btn.innerHTML = 'Click me and check the console!'
     btn.onclick = printMe
     element.appendChild(btn)
