2eae9325ec21344b456113b4356f511bd07f07e2
matengfei1 <matengfei1@hisense.com>
2018-12-11 19:31:33 +0800

    treeshaking :markSideEffect&Minify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import _ from "lodash"
+import {cube} from './math.js'
 
 function component(){
-    let element = document.createElement('div')
+    let element = document.createElement('pre')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = [
+        'Hello webpack',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n')
+    
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const path = require('path')
 module.exports = {
     entry: './src/index.js',
     output:{
-        filename:'main.js',
+        filename:'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    mode: 'production',
+    // optimization: {
+    //     usedExports: true
+    // }
+    // In order to take advantage of tree shaking, you must...
+// 
+    // Use ES2015 module syntax (i.e. import and export).
+    // Ensure no compilers transform your ES2015 module syntax into CommonJS modules (this is the default behavior of popular Babel preset @babel/preset-env - see documentation for more details).
+    // Add a "sideEffects" property to your project's package.json file.
+    // Use production mode configuration option to enable various optimizations including minification and tree shaking.
+
 }
